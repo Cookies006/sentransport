09a1e9024b0b9e880432e3bbb8812feaaa331f97
AMZO S <amysene@esp.sn>
2026-05-18 22:53:17 +0000

    Lab 5 : React consomme Flask via fetch et useEffect

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 import json
-from flask import Flask, jsonify
 from flask_cors import CORS
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 CORS(app)
@@ -32,5 +32,37 @@ def get_ligne(ligne_id):
 
     return jsonify(ligne)
 
+
+
+@app.route("/arrets")
+def get_arrets():
+    tous_les_arrets = set()
+    for ligne in lignes:
+        for arret in ligne["listeArrets"]:
+            tous_les_arrets.add(arret)
+    return jsonify(list(tous_les_arrets))
+
+@app.route("/stats")
+def get_stats():
+    nb_lignes = len(lignes)
+    total_arrets = sum(ligne["arrets"] for ligne in lignes)
+    ligne_max = max(lignes, key=lambda l: l["arrets"])
+    return jsonify({
+        "nombre_de_lignes": nb_lignes,
+        "total_arrets": total_arrets,
+        "ligne_plus_darrets": ligne_max["numero"]
+    })
+
+@app.route("/lignes/recherche")
+def recherche_ligne():
+    q = request.args.get("q", "").lower()
+    resultats = [
+        ligne for ligne in lignes
+        if q in ligne["depart"].lower() or q in ligne["arrivee"].lower()
+    ]
+    return jsonify(resultats)
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
+
